f87e08a369e7159a853f3a853b6b82b156883c91
Sapfir0 <sapfir999999@yandex.ru>
2019-03-31 21:45:47 +0400

    бекэнд готов

--- server/main.js
+++ server/main.js
@@ -1,1 +1,12 @@
-//тупа нихера нет
+var express = require('express');
+var app = express();
+
+// создаём маршрут
+app.get('/', function(req, res) {
+  res.sendfile('./public/index.html');
+});
+
+
+app.listen(7060);
+
+console.log('e');
