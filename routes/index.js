8927f579a73b27257e954163e9ee1ccccccfbc31
Sapfir0 <sapfir999999@yandex.ru>
2019-04-06 20:46:44 +0400

    Мини POST обновление

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var route = express.Router();
-
+const bodyParser = require('body-parser'); 
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 // /* GET home page. */
 route.get('/', (req, res, next) => { //next only for middleware func lel
     res.render('index');
@@ -14,9 +16,15 @@ route.get('/signIn', (req, res, next) => {
     res.render('signIn');
 });
 
-route.post('/signIn/createUser',  (req, res, next) => {
-    //тут будут проверки на стороне сервера
-    
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+route.post("/signIn", urlencodedParser, (req, res) => {
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+    const login = req.body.login;
+    //res.send(email + password + login); 
+    //проверяем есть ли данные поля в нашей бд, и если есть, идем нахер
 });
+
 
 module.exports = route;
