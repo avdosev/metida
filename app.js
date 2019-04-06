cb41dd66e9af5434c3f617dabac8b107cf1bd07a
Sapfir0 <sapfir999999@yandex.ru>
2019-04-06 18:40:13 +0400

    Маршутизация вынесена в отдельный файл Начал разбираться с добавлением favicon, почему-то не робит Начал подключение базы данных для регистрации

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser'); //dont use yet
 const port = 7080;
 //const {check, validationResult} = require('express-validator/check');
-
-//const indexRoute = require('/routes/index'); //пока не хочу роуты выкидыать в отдельный файл
-// const registerRoute = require('/routes/register');
+const route = require('./routes/index');
 
 const app = express();
 
@@ -13,30 +11,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'pug'); 
 
-// app.use('/', indexRoute) //will be generate index.jade
-// app.use('/register', registerRoute) 
 
+const favicon = require('serve-favicon');//пока не робит, в хедере пуга тоже лежит подключение иконки
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
 
-app.get('/', (req, res, next) => { //next only for middleware func lel
-    res.render('index')
-})
-
-app.get('/login', (req, res, next) => {
-    res.render('login');
-});
-
-app.get('/signIn', (req, res, next) => {
-    res.render('signIn');
-});
-
-
-
-app.post('/signIn/createUser',  (req, res, next) => {
-    //тут будут проверки на стороне сервера
-    
-});
-
-
+app.use('/', route);
 
 app.listen(port, () => {
     console.log('Server started on' + port + 'port')
