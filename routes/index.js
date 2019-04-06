b0ccdcab269d1742272571b92f2837bcfbde08d4
Sapfir0 <sapfir999999@yandex.ru>
2019-04-06 23:26:47 +0400

    Мини солт обновление

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var route = express.Router();
 const bodyParser = require('body-parser'); 
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
-// /* GET home page. */
+
+
+
 route.get('/', (req, res, next) => { //next only for middleware func lel
     res.render('index');
 })
@@ -17,12 +19,18 @@ route.get('/signIn', (req, res, next) => {
 });
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-route.post("/signIn", urlencodedParser, (req, res) => {
-    console.log(req.body);
-    const email = req.body.email;
-    const password = req.body.password;
-    const login = req.body.login;
-    //res.send(email + password + login); 
+const bcrypt = require('bcrypt-nodejs');
+route.post("/signIn", urlencodedParser, (req, res, next) => {
+    const reg = {
+        email: req.body.email,
+        login:  req.body.login,
+        password: req.body.password
+    };
+
+    const salt = bcrypt.genSaltSync(10);
+    reg.password = bcrypt.hashSync(reg.password, salt);
+    console.log(reg);
+    res.send(reg); 
     //проверяем есть ли данные поля в нашей бд, и если есть, идем нахер
 });
 
