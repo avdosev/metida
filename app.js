2ed0381a3d5d960fb92fcab645dd3d8000545744
kyplyk <avdosev1@yandex.ru>
2019-04-07 20:20:48 +0400

    чинил то что не сломано

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const port = 7080;
 const route = require('./routes/index');
 
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,15 +14,10 @@ const favicon = require('serve-favicon');//пока не робит, в хеде
 app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
 //почему-то у меня появилась эта херня(потом пропала)
 
-
-
-
-
 app.use('/', route); //маршутизация и обработка запросов
 
 app.listen(port, () => {
     console.log('Server started on ' + port + ' port')
 })
-
 
 module.exports = app; //catch this in /bin/www
