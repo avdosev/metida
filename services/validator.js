c189ab57fb5a4fb3c82542d4282624890945bd35
kyplyk <avdosev1@yandex.ru>
2019-04-07 20:53:16 +0400

    fix validator

--- services/validator.js
+++ services/validator.js
@@ -6,8 +6,8 @@ const validators = {
         body('email').isEmail().normalizeEmail(),
         body('password').not().isEmpty().trim()
             .isLength({min:5}).withMessage('So short')
-            .not().matches(/\d/).withMessage('need number')
-            .matches(/^\d+$/).withMessage("need char"),
+            .matches(/\d/).withMessage('need number')
+            .not().matches(/^\d+$/).withMessage("need char"),
         body('login').not().isNumeric().withMessage('need char! ') 
     ]
 
