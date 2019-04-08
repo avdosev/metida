c523e857e28a1a950727c9647d94180d812e7ab5
kyplyk <avdosev1@yandex.ru>
2019-04-08 20:32:31 +0400

    добавил вход

--- services/validator.js
+++ services/validator.js
@@ -1,16 +1,25 @@
 const { body } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 
-const validators = {  
-    userValidator: [
-        body('email').isEmail().normalizeEmail(),
-        body('password').not().isEmpty().trim()
-            .isLength({min:5}).withMessage('So short')
-            .matches(/\d/).withMessage('need number')
-            .not().matches(/^\d+$/).withMessage("need char"),
-        body('login').not().isNumeric().withMessage('need char! ') 
-    ]
+const emailVaidator = body('email').isEmail().normalizeEmail();
 
+const loginValidator = body('login').not().isEmpty().trim()
+                            .not().isNumeric().withMessage('need char');
+
+const passwordValidator = body('password').not().isEmpty().trim()
+                            .isLength({min:5}).withMessage('so short').trim()
+                            .isLength({max: 255}).withMessage('very big');
+
+const validators = {  
+    userCreateValidator: [
+        emailVaidator,
+        loginValidator,
+        passwordValidator 
+    ],
+    userLoginValidator: [
+        loginValidator,
+        passwordValidator
+    ]
 }
 
 module.exports = validators;
