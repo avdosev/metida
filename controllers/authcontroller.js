6ae4f8817225be45c87be76b7e3ec485f79d1879
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-04-23 00:49:00 +0400

    Вкидываю рабочий коммит (#8)
    * Да, все робит
    * Пришлось что-то удалить, надеюсь ничего не сломал при мердже
    * Обновил пакеты

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -16,14 +16,8 @@ home = (req, res, next) => {
 )};
 
 articles = (req, res, next) => {   
-  var textToInsert = 'Unable to load text.';
-  var nameToInsert = 'Unable to load name.'; // значения по умолчанию
-  // потом надо переделать на sql
-  nameToInsert = "Test " + req.params.id;
-  textToInsert = "Nullam convallis sed risus ac laoreet. Vestibulum venenatis mi vel nisl tempor imper."
-  // res.nameToInsert = nameToInsert;
-  // res.textToInsert = textToInsert;
-  ////ыыыыыы как вынести этот блок, чтобы мы получали на вход эти переменные
+  console.log(req.user.header);//ВСЕ РЕНДЕРИТСЯ В article.js
+  console.log(req.user.content); //ЭТА ФУНКЦИЯ НЕ ИСПОЛЬЗУЕТСЯ
   res.render('post', {authorised : req.isAuthenticated(), name : nameToInsert, text: textToInsert})
 };
 
