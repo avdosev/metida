77143e2d1b06635421aa99ccbc8f3ef1f157f1c0
kyplyk <avdosev1@yandex.ru>
2019-05-09 22:22:59 +0400

    redirect to new article

--- controllers/respondent.js
+++ controllers/respondent.js
@@ -6,8 +6,20 @@ function getTopArticles(req, res, next) {
     res.json(res.values.TopArticles);
 }
 
+function redirectToArticle(req, res, next) {
+    // по идее этого здесь быть не должно но тогда сервак станет еще более модульным что не совсем хорошо так что пусть будет здесь
+    if (!res.values.SuccessPushArticle) {
+        res.render('error_page')
+        return ;
+    }
+    const id = res.values.PostId
+    const url = `/post/${id}`
+    res.redirect(url)
+}
+
 module.exports = {
     getComments,
     //pushComment,
-    getTopArticles
+    getTopArticles,
+    redirectToArticle
 };
