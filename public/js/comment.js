094a650b9360a5bf0d43437e5d3628634a559a87
Sapfir0 <sapfir999999@yandex.ru>
2019-05-11 21:37:07 +0400

    ХМ2

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,38 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const post = document.querySelector('.post_text')
+    const post = document.querySelector('.post_text');
     const id = post.id;
-    fetch(`/post/${id}/comments`).then(value => {
-        console.log(value);
-        return value.json()
-    }).then((json => {
-        console.log(json)
-        const insertElem = document.querySelector('.comments_lenta')
-        insertsComments(json, insertElem);
-    })).catch(error => {
-        console.error(error);
-    })
-})
+    fetch(`/post/${id}/comments`)
+        .then(value => {
+            console.log(value);
+            return value.json();
+        })
+        .then(json => {
+            console.log(json);
+            const insertElem = document.querySelector('.comments_lenta');
+            insertsComments(json, insertElem);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 
+    const comment = document.querySelector('#comment');
+    const commentError = document.querySelector('.commentError');
+    comment.addEventListener('input',event => {
+            //тут ошибка, он неправильно считывает регулярку
+            console.log(comment.validity.valid);
+            if (comment.validity.valid) {
+                commentError.innerHTML = '';
+                commentError.className = 'error';
+            } else {
+                commentError.innerHTML =
+                    'Коммент уж слишком маленький. Прям как ';
+                commentError.className = 'error active';
+            }
+        },
+        false
+    );
+
+    document.addEventListener('submit',event => {
+            console.log('ЖМЯК');
+            if (!comment.validity.valid) {
+                commentError.innerHTML = 'ОМАГАД';
+                commentError.className = 'error active';
+                event.preventDefault();
+            }
+        },
+        false
+    );
+});
 
 function insertsComments(objCommentArray, insertedElem) {
     for (let i = 0; i < objCommentArray.length; i++) {
         const objComment = objCommentArray[i];
 
         if (objComment.answeringId === null) {
-            insertComment(objComment, insertedElem)
+            insertComment(objComment, insertedElem);
         } else {
-            const childCommentElem = document.querySelector(`#child_comment_${objComment.answeringId}`)
+            const childCommentElem = document.querySelector(
+                `#child_comment_${objComment.answeringId}`
+            );
             insertComment(objComment, childCommentElem);
         }
     }
 }
-const currentDate = new Date()
+const currentDate = new Date();
 function insertComment(objComment, insertedElem) {
     //const AuthorUrl = ``
-    const Author = objComment.author
-    const Text = objComment.text
-    const Id = objComment.id
-    const date = new Date(objComment.createdAt)
+    const Author = objComment.author;
+    const Text = objComment.text;
+    const Id = objComment.id;
+    const date = new Date(objComment.createdAt);
     const DateStr = DateToStr(date);
 
     // впринципе можно менять
@@ -45,39 +77,43 @@ function insertComment(objComment, insertedElem) {
         <div class = "comment_text"><p>${Text}</p></div>
         <a class="reply" href=#>Ответить</a> 
         <div class = "comment_childer" id = "child_comment_${Id}"></div>
-    </div>
-    `
-    insertedElem.insertAdjacentHTML("beforeend", htmlPost);
+    </div>`;
+    insertedElem.insertAdjacentHTML('beforeend', htmlPost);
 }
 
 function DateToStr(date) {
-    const now = new Date()
+    const now = new Date();
     let str;
-    const nearlyDay = date.getYear() == now.getYear() && date.getMonth() == now.getMonth()
-    const subDay = now.getDate() - date.getDate()
-    
-    if (nearlyDay && subDay == 1) { // вчера
-        str = "вчера "
-    } else if (nearlyDay && subDay == 0) { // сегодня
-        str = "сегодня "
+    const nearlyDay =
+        date.getYear() == now.getYear() && date.getMonth() == now.getMonth();
+    const subDay = now.getDate() - date.getDate();
+
+    if (nearlyDay && subDay == 1) {
+        // вчера
+        str = 'вчера ';
+    } else if (nearlyDay && subDay == 0) {
+        // сегодня
+        str = 'сегодня ';
     } else {
         month = [
-            "Января",
-            "Февраля",
-            "Марта",
-            "Апреля",
-            "Мая",
-            "Июня",
-            "Июля",
-            "Августа",
-            "Сентября",
-            "Октября",
-            "Ноября",
-            "Декабря"
-        ]
-        str = `${date.getDate()} ${month[date.getMonth()-1]} ${date.getFullYear()}`
+            'Января',
+            'Февраля',
+            'Марта',
+            'Апреля',
+            'Мая',
+            'Июня',
+            'Июля',
+            'Августа',
+            'Сентября',
+            'Октября',
+            'Ноября',
+            'Декабря'
+        ];
+        str = `${date.getDate()} ${
+            month[date.getMonth() - 1]
+        } ${date.getFullYear()}`;
     }
 
-    str += ' в ' + date.toLocaleTimeString()
+    str += ' в ' + date.toLocaleTimeString();
     return str;
 }
