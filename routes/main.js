b2088174b85570b58a36b7f4e5ea7bc18c68ada4
kyplyk <avdosev1@yandex.ru>
2019-05-14 22:39:05 +0400

    АДАПТИВ

--- routes/main.js
+++ routes/main.js
@@ -52,6 +52,7 @@ const initAuthControllers = (app, passport) => {
     
     app.post(
         '/createArticle',
+        isLoggedIn,
         urlencodedParser,
         articleValidator,
         /* отправить на модерацию */
