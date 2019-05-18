31afd280ce4b5445c6bf1929711d56e8629855d2
Sapfir0 <sapfir999999@yandex.ru>
2019-05-18 12:13:29 +0400

    воть лента

--- public/js/lenta.js
+++ public/js/lenta.js
@@ -1,27 +1,44 @@
+var articlesCount = 1 //число статей, которые будут на странице до нажатия кнопки
+//пока при нажатии подаются оставшиеся статьи
+
 document.addEventListener('DOMContentLoaded', () => {
     const getMoreArticles = document.querySelector(".getMoreArticles")
-   
+    getArticle(articlesCount)
     getMoreArticles.addEventListener("click", event => {      
-        fetch("/getMoreArticles").then(res => {
-            
-        })
+        getArticle(articlesCount)
     })
+        
+})
 
 
-
+var currentCountOfArticles = 0; //мини костылек, не смотри сюда //это статическая переменная
+/*
+Если подается true, или аргументы пустуют, то мы обходим весь джсон
+Если подается false, то нужно подать еще и число обхода
+*/
+function getArticle(articlesCount=0) {
     fetch('./top').then(value => {
         console.log(value);
         return value.json()
     }).then((json => {
         console.log(json)
         const insertElem = document.querySelector('.lenta')
-        for (let i in json) {
+        if (currentCountOfArticles >= json.length ){
+            console.error("Загружены все записи")
+            return
+        }
+        for (let i=currentCountOfArticles; i<articlesCount+currentCountOfArticles; i++) { 
+            if (json[i] == undefined) {
+                console.error("Все")
+                break
+            }
             insertPostPreview(json[i], insertElem);
         }
+        currentCountOfArticles+=articlesCount;  
     })).catch(error => {
         console.error(error);
     })
-})
+}
 
 function insertPostPreview(objPost, insertedElem) {
     const url = `/post/${objPost.id}`
