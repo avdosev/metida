9c198afaa00011e3a01745f2c0fc4fc97bd8bbef
kyplyk <avdosev1@yandex.ru>
2019-05-18 21:29:32 +0400

    update2

---
+++ controllers/page.js
@@ -0,0 +1,58 @@
+const register = (req, res ) => {
+    res.render('register', { authorised: req.isAuthenticated() });
+};
+
+const signin = (req, res ) => {
+    res.render('sign_In', { authorised: req.isAuthenticated() });
+};
+
+const createArticle = (req, res) => {
+    res.render('create_article', { authorised: req.isAuthenticated() });
+};
+
+const home = (req, res ) => {
+    res.render('home', { authorised: req.isAuthenticated() });
+};
+
+const article = (req, res ) => {
+    res.render('post', {
+        authorised: req.isAuthenticated(),
+        name: res.values.article.header,
+        text: res.values.article.content,
+        id:   res.values.article.id
+    });
+};
+
+const logout = (req, res ) => {
+    req.session.destroy(err => {
+        res.redirect('/');
+    });
+};
+
+const index = (req, res ) => {
+    res.render('index', {
+        authorised: req.isAuthenticated()
+    });
+};
+
+const errorPage = (req, res ) => {
+    res.render('error_page');
+};
+
+const authorProfile = (req, res ) => {
+    console.log(req.values)
+    res.render('profile', { authorised: req.isAuthenticated() });
+}
+
+
+module.exports = {
+    register,
+    signin,
+    home,
+    createArticle,
+    errorPage,
+    index,
+    article,
+    logout,
+    authorProfile
+};
