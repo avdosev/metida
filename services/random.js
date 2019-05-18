cef93cf79732c815b38f198df8cacbbd1b5fa6f8
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-05-18 15:21:25 +0400

    Seeders (#34)
    * seeders
    * rows

---
+++ services/random.js
@@ -0,0 +1,20 @@
+
+function stringGenerator(len) {
+    const chrs = 'ab deh kmnp swxzA BD EF GHK MNPQR STWX Z123 4567 89';
+    var str = '';
+    for (var i = 0; i < len; i++) {
+        var pos = Math.floor(Math.random() * chrs.length);
+        str += chrs.substring(pos,pos+1);
+    }
+    return str;
+}
+
+function randInt(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+module.exports = {
+    stringGenerator,
+    randInt
+}
