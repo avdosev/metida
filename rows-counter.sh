d9c23f01ea826837519e5a3978948c2bf924fdea
Sapfir0 <sapfir999999@yandex.ru>
2019-05-22 00:30:44 +0400

    надо подправить баш

--- rows-counter.sh
+++ rows-counter.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+#RawAll=$(find . -not -path "./node_modules/*" -not -path "./rows-counter.sh" -not -path "./README.md" -not -path "./public/img/*" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./public/js/ckeditor/*" -not -name "package*" -type f | xargs wc -l | grep "total") #тут пути, которые не учитываются ни в одном из рейтингов
 
-excludeDir = "./node_modules/*", "./public/img/*", "./LICENSE", "./.git/*", "./public/js/ckeditor/*", "package*" #пока не придумал как юзнуть абстрактно
+#Rawfront=$(find ./public ./views -not -path "./public/json/*" -not -path "./public/img/*" -not -path "./public/js/*" -type f | xargs wc -l | grep "total")  
+#find ./public ./views -not -path "./public/json/*" -not -path "./public/img/*" -not -path "./public/js/*" -type f | xargs wc -l 
+RawAll=$(find . -not -path "./node_modules/*" -not -path "./rows-counter.sh" -not -path "./README.md" -not -path "./public/img/*" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./public/js/ckeditor/*" -not -name "package*" -type f | xargs wc -l | grep "total") #тут пути, которые не учитываются ни в одном из рейтингов
 
-find . -not -path "./node_modules/*" -not -path "./README.md" -not -path "./public/img/*" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./public/js/ckeditor/*" -not -name "package*" -type f | xargs wc -l | grep "total"
+Rawfront=$(find ./public ./views -not -path "./public/js/ckeditor/*" -type f | xargs wc -l | grep "total") #будем считать что фронтендеры написали /public/ и /views (забудем про севрерный рендеринг)
+
+
+echo $Rawfront
+allRows=$(expr match "$RawAll" '\(.[0-9]*\)') 
+frontEnd=$(expr match "$Rawfront" '\(.[0-9]*\)')  # число останется только 
+backEnd=$(expr $allRows - $frontEnd)
+
+echo "Накал теч родил$allRows строк"
+echo "На фронте написали$frontEnd строк"
+echo "На беке написали $backEnd"
+
+if [[ $backEnd -gt  $frontEnd ]]; then
+    echo "Бекендеры снова перевыполняют план"
+elif [[ $backEnd -lt $frontEnd ]]; then
+    echo "Фронтендеры вперед!"
+else
+    echo "Omnes pares sunt, coram Deo"
+fi
