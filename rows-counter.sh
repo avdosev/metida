d21a977ad6d01cbaa1932970e928fa94b9d7d446
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-05-23 21:25:36 +0300

    Конструктива 0 (#43)

--- rows-counter.sh
+++ rows-counter.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-#RawAll=$(find . -not -path "./node_modules/*" -not -path "./rows-counter.sh" -not -path "./README.md" -not -path "./public/img/*" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./public/js/ckeditor/*" -not -name "package*" -type f | xargs wc -l | grep "total") #тут пути, которые не учитываются ни в одном из рейтингов
 
-#Rawfront=$(find ./public ./views -not -path "./public/json/*" -not -path "./public/img/*" -not -path "./public/js/*" -type f | xargs wc -l | grep "total")  
-#find ./public ./views -not -path "./public/json/*" -not -path "./public/img/*" -not -path "./public/js/*" -type f | xargs wc -l 
+#Скрипт для лулзов и посмеяться над кем-нибудь
+
 RawAll=$(find . -not -path "./node_modules/*" -not -path "./rows-counter.sh" -not -path "./README.md" -not -path "./public/img/*" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./public/js/ckeditor/*" -not -name "package*" -type f | xargs wc -l | grep "total") #тут пути, которые не учитываются ни в одном из рейтингов
 
-Rawfront=$(find ./public ./views -not -path "./public/js/ckeditor/*" -type f | xargs wc -l | grep "total") #будем считать что фронтендеры написали /public/ и /views (забудем про севрерный рендеринг)
+RawDesigners=$(find ./public ./views -not -path "./public/json/*" -not -path "./public/img/*" -not -path "./public/js/*" -type f | xargs wc -l | grep "total")
 
-
-echo $Rawfront
-allRows=$(expr match "$RawAll" '\(.[0-9]*\)') 
-frontEnd=$(expr match "$Rawfront" '\(.[0-9]*\)')  # число останется только 
-backEnd=$(expr $allRows - $frontEnd)
+allRows=${RawAll/total/}
+designersRows=${RawDesigners/total/}
+codersRows=$(expr $allRows - $designersRows)
 
 echo "Накал теч родил$allRows строк"
-echo "На фронте написали$frontEnd строк"
-echo "На беке написали $backEnd"
+echo "Дизайнеры написали$designersRows строк"
+echo "Кодеры написали $codersRows"
 
-if [[ $backEnd -gt  $frontEnd ]]; then
+if [[ $codersRows -gt  $designersRows ]]; then
     echo "Бекендеры снова перевыполняют план"
-elif [[ $backEnd -lt $frontEnd ]]; then
+elif [[ $codersRows -lt $designersRows ]]; then
     echo "Фронтендеры вперед!"
 else
     echo "Omnes pares sunt, coram Deo"
