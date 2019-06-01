5a61e3162f38d3907a8d4cd6dd5c33ebfa6a8bb3
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-06-01 15:47:36 +0400

    хе я влетаю с введением фроненд валидации(а как она поможет, если люди на бекэнде чилят) (#46)
    если я что-то сломал, то надеюсь нет

--- public/js/article_page.js
+++ public/js/article_page.js
@@ -1,18 +1,87 @@
-console.log("слышъ, писать статью")
+const validators = { // все еще не понимаю как вынести это все
+    strHeaderError: 'Заголовок должен быть длиннее', 
+    strDisclaimerError: 'Дисклеймер должен быть длиннее', 
+    strContentError: 'Контент должен быть длиннее', 
+
+    strEventHeaderError: 'Тебе не стыдно?', 
+    strDisclaimerEventError: 'Почему ты такой немногословный?',
+    strContentEventError: 'Все совсем плохо? Напиши мне на почту, побеседуем',
+
+    headerRegExp: new RegExp('.{10,}'), //это должно конечно же повторяться в хтмл файле для css изменений
+    disclaimerRegExp: new RegExp('.{10,}'),
+    conentRegExp: new RegExp('.{10,}')
+}
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const checkbox = document.getElementById("previews");
-    const textarea = document.querySelector('#article')
     const header = document.querySelector('#header')
     const disclaimer = document.querySelector('#disclaimer')
+    const content = document.querySelector('#article')
+    const submitBtn = document.querySelector('#submit')
     
     checkbox.addEventListener("click", showArtIfCheckboxCheked)
-    textarea.addEventListener('change', showArtIfCheckboxCheked)
+    content.addEventListener('change', showArtIfCheckboxCheked)
     header.addEventListener('change', showArtIfCheckboxCheked)
 
+    const headerError = document.querySelector(".headerError")
+    const disclaimerError = document.querySelector(".disclaimerError")
+    const contentError = document.querySelector(".contentError")
+
+
+    function showError(spanError, str) {
+        spanError.innerHTML = str;
+        spanError.className = 'error active';
+    }
+
+    function hideError(spanError) {
+        spanError.innerHTML = '';
+        spanError.className = 'error';
+    }
+
+    function checkValidation(spanError, widget, regExp, strError) {
+        if (!widget.value.match(regExp) )  { 
+            showError(spanError, strError)
+        }
+        else {
+            hideError(spanError)
+        }
+    }
+
     header.addEventListener("input", () => {
-        
+        checkValidation(headerError, header, validators.headerRegExp, validators.strHeaderError)
     })
+
+    disclaimer.addEventListener("input", () => {
+        checkValidation(disclaimerError, disclaimer, validators.disclaimerRegExp, validators.strDisclaimerError)
+    })
+
+    content.addEventListener("input", () => {
+        checkValidation(contentError, content, validators.contentRegExp, validators.strContentError)
+    })
+
+
+    submitBtn.addEventListener('click', (event) => {
+        if ( !header.value.match(validators.headerRegExp) )  {
+            showError(headerError, validators.strEventHeaderError)
+            event.preventDefault();
+        }
+        else if(!disclaimer.value.match(validators.disclaimerRegExp) ) {
+            showError(disclaimerError, validators.strDisclaimerEventError)
+            event.preventDefault();
+        }
+        else if(!content.value.match(validators.contentRegExp) ) {
+            showError(contentError, validators.strContentEventError)
+            event.preventDefault();
+        }
+        else { // валидация на фронте пройдена, делаем запрос к серверу и смотрим на его ответ
+
+
+        }
+    },
+    false
+);
 
     function showArtIfCheckboxCheked() {
         let headerStr = '', textStr = '';
