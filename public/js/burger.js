d1be9be6eebc8ddd06b2e29de91ffc9f9f380d7f
Sapfir0 <sapfir999999@yandex.ru>
2019-06-07 19:48:09 +0400

    чето все раскоммитились

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,18 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', () =>{
-    document.addEventListener('click', function(e){
-        if (e.target.id === "burger")
-        {
-            if (document.getElementsByClassName('submenu')[0].id === "submenu")
-            {
-                document.getElementsByClassName('submenu')[0].id = "submenu__active";
+    const burger = document.getElementsByClassName('submenu')[0]
+    document.addEventListener('click', (event) => {
+        
+        if (event.target.id === "burger")  {
+            if (burger.id === "submenu")  {
+                burger.id = "submenu__active";
+            } 
+            else {
+                burger.id = "submenu"; 
             }
-            else{
-                document.getElementsByClassName('submenu')[0].id = "submenu"; 
-            }
-        }
-        else{
-            document.getElementsByClassName('submenu')[0].id = "submenu";
+        } 
+        else  {
+            burger.id = "submenu";
         }
     });
 });
