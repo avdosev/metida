ea069a64384937a85455930c55dc73faa3c5d2ce
kyplyk <avdosev1@yandex.ru>
2019-06-07 14:36:28 +0400

    refactoring on backend

--- database/models/images.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Image = sequelize.define('images', {
-        id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
-        image: { type: Sequelize.BLOB, allowNull: false }
-        //pathToImg: { type:Sequelize.VARCHAR, allowNull: false}
-    });
-
-    return Image;
-};
