9d4dc030b79ca576d55645138e1b18124e158c2b
Sapfir0 <sapfir999999@yandex.ru>
2019-06-11 10:44:54 +0400

    надеюсь я ничего не сломал

--- config/index.js
+++ config/index.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const dotenv = require("dotenv")
 
-const port = process.env.PORT || 7080;
+const production= ((process.env.NODE_ENV === "production") ? true : false)
+if (!production) {
+    dotenv.load()
+}
+
+const port = process.env.PORT;
 const url = `localhost:${port}`;
 const mainDir = path.join(__dirname, '..');
 const imgDir = mainDir + '/public/img';
-const secretKey = 'keyboard cat'
-const production= ((process.env.NODE_ENV === "production") ? true : false)
+const secretKey = process.env.SECRET_KEY
 
 const mail = require('./mail')
 const messages = require('./messages')
