73e25f1c3ace18c8730b7012bb856acce7562c4d
Sapfir0 <sapfir999999@yandex.ru>
2019-06-22 20:09:06 +0400

    мерджевый аутизм

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,9 @@
 import { DateToStr } from "/public/js/modules/dateRU.js";
 import { showError, hideError } from "/public/js/modules/input_error.js"
 
+let post;	
+let id;	
+
 document.addEventListener('DOMContentLoaded', async () => {
 
     post = document.querySelector('.post_text');
