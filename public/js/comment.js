3f4e911bee9446c42aa10c5bafb1b71679203f56
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-06-22 20:03:47 +0400

    Development (#48)
    * ща запушится целая ветвь(опять не корректно)

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,43 +1,36 @@
-import { DateToStr } from "dateRU";
+import { DateToStr } from "/public/js/modules/dateRU.js";
+import { showError, hideError } from "/public/js/modules/input_error.js"
 
-let post;
-let id;
+document.addEventListener('DOMContentLoaded', async () => {
 
-
-function showError(spanError, str) {
-    spanError.innerHTML = str;
-    spanError.className = 'error active';
-}
-
-function hideError(spanError) {
-    spanError.innerHTML = '';
-    spanError.className = 'error';
-}
-
-document.addEventListener('DOMContentLoaded', () => {
     post = document.querySelector('.post_text');
     id = post.id;
+
     const commentError = document.querySelector('.commentError');
     const comment = document.querySelector('.comment_area') //я не могу с жить с ошибкой
     const sendCommentBtn = document.querySelector(".EnterButton")
     
-    fetch(`/post/${id}/comments`)
-        .then(value => {
-            console.log(value);
-            return value.json();
-        })
-        .then(json => {
-            console.log(json);
-            const insertElem = document.querySelector('.comments_lenta');
-            insertsComments(json, insertElem);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    fetch(`/post/${id}/comments`).then(value => {
+        console.log(value);
+        return value.json();
+    }).then(json => {
+        console.log(json);
+        const insertElem = document.querySelector('.comments_lenta');
+        insertsComments(json, insertElem);
+    }).catch(error => {
+        console.error(error);
+    });
+
+    const validators = await fetch('/public/json/input_errors.json').then(response => {
+        if (response.ok)
+            return response.json()
+        else 
+            console.log('с джсоном какая то проблема', response)
+    })
 
     sendCommentBtn.addEventListener("click", (event) => {
-        if ( !comment.value.match(comment1.commentRegExp)  )  {
-            commentError.innerHTML = comment1.commentEventError;
+        if ( !comment.value.match(validators.comment.regexp)  )  {
+            commentError.innerHTML = validators.comment.EventError[0];
             commentError.className = 'commentError error active';
              //не пускаем его дальше
         }
@@ -47,28 +40,20 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     comment.addEventListener('input', () => {
-            if (comment.value.match(comment1.commentRegExp)) { 
+            if (comment.value.match(validators.comment.regexp)) { 
                 hideError(commentError)
             } else {
-                showError(commentError, comment1.commentError)
+                showError(commentError, validators.comment.Error)
             }
         },
         false //объясни потом, что значит этот бул
     );
-
-
 });
 
-function refreshPage() {
+function refreshComments() {
+    // TODO правильная подгрузка коммментов
+    // загружаем заново комменты и тех что нет инсертим
     window.location.reload();
-}
-
-function returnToArticle() { //в общем, вызвать это дерьмо, когда он начинает переводить на джсон
-    document.location.href = window.location.href.replace(new RegExp("/pushComment.*"), "")
-}
-
-function getAnsweringId() {
-    
 }
 
 function responseComment(commentText, answeringId = null) {
@@ -85,7 +70,7 @@ function responseComment(commentText, answeringId = null) {
     fetch(`/post/${id}/pushComment`, 
         options
     ).then(() => {
-        refreshPage()
+        refreshComments()
     }).catch(err => {
         // TODO: обработка ошибки
         console.error(err)
@@ -122,15 +107,19 @@ function insertComment(objComment, insertedElem) {
             <time class = "created_commit">${DateStr}</time>
         </div>
         <div class = "control_block">
-            <button class = "reply comment_control" data-type="create" style = "display: inline" onclick="createClick(${Id})"></button>
-            <button class = "reply comment_control" data-type="cancel" style = "display: none" onclick="cancelClick(${Id})"></button>
+            <button class = "reply comment_control" data-type="create" style = "display: inline" id="createBtn_${Id}"></button>
+            <button class = "reply comment_control" data-type="cancel" style = "display: none" id="cancelBtn_${Id}"></button>
             <button class = "updateComment GreyButton">Редактировать</button>
             <button class = "removeComment GreyButton">Удалить</button>
         </div> 
         <div class = "comment_text">${Text}</div>
         <div class = "comment_childer" id = "child_comment_${Id}"></div>
     </div>`;
+
     insertedElem.insertAdjacentHTML('beforeend', htmlPost);
+
+    document.querySelector(`#createBtn_${Id}`).addEventListener('click', () => {createClick(Id)})
+    document.querySelector(`#cancelBtn_${Id}`).addEventListener('click', () => {cancelClick(Id)})
 }
  
 // можно по другому но пока так
@@ -142,10 +131,10 @@ function createClick(id) {
     control_block.querySelector('button[data-type=cancel]').style.cssText = 'display: inline';
     
     const reply_block = `
-    <div class = "comment reply_comment">
+    <div class = "comment reply_comment" id="reply_comment_${id}">
         <textarea class = "comment_area" id="comment_area_${id}" name="comment" cols="30" rows="10" required='required' pattern='.{10,}'></textarea>
         <div class = "button">
-            <button class = "EnterButton" onclick="responseComment(document.querySelector('#comment_area_${id}').value, ${id})">
+            <button class = "EnterButton">
                 Отправить
             </button>
         </div>
@@ -153,6 +142,8 @@ function createClick(id) {
     </div>
     `
     insert_block.insertAdjacentHTML('beforebegin', reply_block)
+
+    document.querySelector(`#reply_comment_${id} .EnterButton`).addEventListener('click', () => { responseComment(document.querySelector(`#comment_area_${id}`).value, id) })
 }
 
 function cancelClick(id) {
