9473cd5c6d425d73c8d0aa3e9bbc90a31480f618
kyplyk <avdosev1@yandex.ru>
2019-06-26 19:08:49 +0400

    need modules

--- public/js/post.js
+++ public/js/post.js
@@ -1,6 +1,3 @@
-import { highLighter } from "./modules/highlighter.js";
-import { getData } from "./modules/helper.js";
-
 document.addEventListener('DOMContentLoaded', start);
 
 function getArticleId(url) {
@@ -36,19 +33,4 @@ function start() {
         })
     })
 
-    // тупа хайлайт кода
-    async function highlighting() {
-        const elems = document.querySelectorAll('code');
-        if (!elems) {
-            return;
-        }
-        
-        const lexTable = await getData('/public/json/lexem_table.json')
-        for (let i = 0; i < elems.length; i++) {
-            elems[i].innerHTML = highLighter(elems[i].innerText, lexTable);
-        }
-        
-    }
-    highlighting();
-    
 }
