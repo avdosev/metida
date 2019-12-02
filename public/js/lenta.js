3f4d82afa150ce6bb9a4068a6ffd4359640a6bf7
kyplyk <avdosev1@yandex.ru>
2019-12-02 12:26:06 +0400

    profile work2

--- public/js/lenta.js
+++ public/js/lenta.js
@@ -1,3 +1,5 @@
+import { insertPostPreview } from "./modules/articles.js"
+
 const articlesCount = 10; //число статей, которые будут на странице до нажатия кнопки
 //пока при нажатии подаются оставшиеся статьи
 document.addEventListener('DOMContentLoaded', () => {
@@ -42,22 +44,5 @@ function getArticle(articlesCount=0) {
     })
 }
 
-function insertPostPreview(objPost, insertedElem) {
-    const url = `/post/${objPost.id}`;
-    const htmlPost = `
-    <div class = "post">
-        <div class="title">
-            <a href = "${url}"><h3>${objPost.header}</h3></a>
-        </div>
-        <div class="disclaimer">
-            <p>${objPost.disclaimer}</p>
-        </div> 
-        <div class="after_post">
-            <a href="${url}" class="BtnToArticle">Читать дальше</a>
-            <a href="${url+"#comments"}" class="BtnToArticleComments"><img class="after_post_icon" src="/public/img/ui_icon/comment.svg"></a>
-        </div>
-    </div>
-    `;
-    insertedElem.insertAdjacentHTML("beforeend", htmlPost);
-}
+
 
