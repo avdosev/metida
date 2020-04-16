299827dd3c93da8f8e4e4e60098ffea84e07d1e3
avdosev <avdosev1@yandex.ru>
2020-04-16 13:56:04 +0400

    Исправил неявные вещи в работе с Sequelize + правка кодстайла

--- config/index.js
+++ config/index.js
@@ -3,8 +3,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
-const production= process.env.NODE_ENV === "production";
-if (!production) {
+if (!process.env.NODE_ENV) {
     dotenv.load()
 }
 
@@ -15,7 +14,9 @@ const port = process.env.PORT;
 const url = `localhost:${port}`;
 const mainDir = path.join(__dirname, '..');
 const imgDir = mainDir + '/public/img';
-const secretKey = process.env.SECRET_KEY
+const secretKey = process.env.SECRET_KEY;
+const serverType = process.env.NODE_ENV;
+const production = serverType === "production";
 
 const readJson = (filename) => JSON.parse(fs.readFileSync(`${__dirname}/${filename}`).toString());
 const mail = readJson('mail.json');
@@ -29,5 +30,6 @@ export default {
     secretKey,
     production,
     mail,
-    messages
+    messages,
+    serverType
 };
