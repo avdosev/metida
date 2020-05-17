4dc52fb834af83a43dbd871624e7fec2072464f1
Sapfir0 <sapfir999999@yandex.ru>
2020-05-17 14:05:04 +0400

    Верстка главной страницы

---
+++ src/components/Pages/Index/Index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../main.css"
+import "../../lenta.css"
+import "../../colors.css"
+
+
+
+
+export default function Index() {
+    return(<>
+        <div className="layout_body"> </div>
+        <div className="content.content"> </div>
+        <h1>Умная лента</h1>
+        <hr className="head" />
+        <div className="lenta"> </div>
+        <button className="getMoreArticles">Показать больше</button>
+
+        </>
+    )
+}
+
+
+
+
+
+
+
+
