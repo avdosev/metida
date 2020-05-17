4b951965026ef4dc46d3c964c439a2b27c131bff
Sapfir0 <sapfir999999@yandex.ru>
2020-05-17 12:45:39 +0400

    Добавил реакт в проект

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+
+
+function App() {
+    return (
+        <Router>
+
+        </Router>
+    );
+}
+
+export default App;
