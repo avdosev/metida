cbc00afc193e3238d3e7c4b7b020a36efd6843a5
Sapfir0 <sapfir999999@yandex.ru>
2020-05-22 00:48:25 +0400

    Lthmvj

--- src/components/Pages/Post/PostPage.tsx
+++ src/components/Pages/Post/PostPage.tsx
@@ -3,15 +3,15 @@ import "../../input.css"
 import "../../main.css"
 import "./post.css"
 import "../../comments.css"
-import { highlightArrayOfCodeElems } from "./highlighter.js";
-import { refreshComments, responseComment, getArticleId } from './comments.js';
+import {highlightArrayOfCodeElems} from "./highlighter.js";
+import {getArticleId, loadComments} from './comments.js';
 import SimplePage from "../../Templates/SimplePage";
 import Header from "../../Molecules/Header/Header";
 import RegisterForm from "../../Organisms/RegisterForm/RegisterForm";
 import Footer from "../../Organisms/Footer/Footer";
-import {logger} from "sequelize/types/lib/utils/logger";
 import {get} from "../../Router";
-
+import CommentLenta from "../../Molecules/CommentLenta/CommentLenta";
+import {Comment} from "../../Atoms/Comment/Comment";
 
 interface IProps {
 
@@ -20,16 +20,18 @@ interface IProps {
 interface IState {
     header: string,
     disclaimer: string,
-    content: string
+    content: string,
+    comments: Array<JSX.Element>
 }
 
 
-export default class PostPage extends React.Component<IProps, IState>{
+export default class PostPage extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
-        this.state = {header: '', content: '', disclaimer: ''}
+        this.state = {header: '', content: '', disclaimer: '', comments: []}
 
     }
+
     deleteArticle = async () => {
         const res = await fetch(`/api/post/${getArticleId()}`, {
             method: "delete"
@@ -47,46 +49,69 @@ export default class PostPage extends React.Component<IProps, IState>{
 
     async componentDidMount() {
         const article = await get(`/api/post/${getArticleId()}`)
-        this.setState({name: article.name, ...article})
-        console.log(this.state)
+        this.setState({...article})
+
+        let comments = await loadComments(getArticleId())
+        let lenta = []
+        console.log(comments)
+        comments = comments.sort((a: any, b: any) => {
+            if (a.id < b.id) {
+                return -1;
+            }
+            if (a.id > b.id) {
+                return 1;
+            }
+            return 0
+        })
+        console.log(comments)
+
+        for (const comment of comments) {
+            if (comment.answeringId === null) {
+                lenta.push(<Comment key={comment.id} comments={comment}/>)
+            } else {
+                const IdChildElem = `#child_comment_${comment.answeringId}`
+                lenta.push(<div className="comment_childer" id={IdChildElem}><Comment key={comment.id}
+                                                                                      comments={comment}/></div>)
+            }
+        }
+
+        this.setState({comments: lenta})
     }
+
+
 
     render() {
         console.log(this.props)
-        return (<SimplePage header={<Header />} content={ <div className="layout_body">
+        return (<SimplePage header={<Header/>} content={<div className="layout_body">
             <div className="content">
                 <button className="deleteAricleLink" onClick={this.updateArticle}>Удалить статью</button>
                 <button className="updateAricleLink" onClick={this.deleteArticle}>Редактировать статью</button>
                 <article className="post_text">
-                    <h1 dangerouslySetInnerHTML={{__html: this.state.header}} />
-                    <p dangerouslySetInnerHTML={{__html: this.state.disclaimer}} />
-                    <p dangerouslySetInnerHTML={{__html: this.state.content}} />
+                    <h1 dangerouslySetInnerHTML={{__html: this.state.header}}/>
+                    <p dangerouslySetInnerHTML={{__html: this.state.disclaimer}}/>
+                    <p dangerouslySetInnerHTML={{__html: this.state.content}}/>
                 </article>
                 <div className="comments_lenta onfullwidth" id="comments">
                     <h3>Комментарии:</h3>
+                    <CommentLenta>
+                        {this.state.comments}
+                    </CommentLenta>
                 </div>
                 <div className="new_comment_block">
                     <p>Зарегистрируйся, если хочешь оставить коммент</p>
                 </div>
             </div>
-        </div>} footer={<Footer/>} />)
+        </div>} footer={<Footer/>}/>)
 
     }
 }
 
-//
-// import { highlightArrayOfCodeElems } from "../../../js/modules/highlighter.js";
-//
-// import { showError, hideError, checkValidationWithRegExp } from "../input_error.js"
-// import { refreshComments, responseComment, getArticleId } from '../../../js/modules/comments.js';
-//
 // document.addEventListener('DOMContentLoaded', start);
 //
 // async function start() {
 //     const codeElems = document.querySelectorAll('code');
 //     highlightArrayOfCodeElems(codeElems);
 //
-
 //     const commentError = document.querySelector('.commentError');
 //     const comment = document.querySelector('.comment_area') //я не могу с жить с ошибкой
 //     const sendCommentBtn = document.querySelector(".EnterButton")
