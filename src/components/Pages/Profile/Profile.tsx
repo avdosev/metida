fa68d102829ea6c2b4d56c1cb2ba63c164bf71c2
Sapfir0 <sapfir999999@yandex.ru>
2020-05-22 01:45:49 +0400

    наверно, все сломалось

--- src/components/Pages/Profile/Profile.tsx
+++ src/components/Pages/Profile/Profile.tsx
@@ -5,11 +5,26 @@ interface IState {
 }
 
 interface IProps {
-
+    username: string
 }
 
 export default class Profile extends React.Component<IProps, IState> {
     render() {
-        return undefined;
+        return (
+            <div className="layout_body">
+                <h1>Профиль</h1>
+                <div className="profile">
+                    <div className="left">
+                        <img className="avatar" src={process.env.PUBLIC_URL + "/img/default/avatar_small.png"}/>
+                    </div>
+                    <div className="right">
+                        <h3>{this.props.username}</h3>
+                        <p className="about">Обо мне:
+                            Ты ничего не узнаешь</p>
+                    </div>
+                </div>
+                <div className="lenta"></div>
+            </div>)
     }
 }
+
