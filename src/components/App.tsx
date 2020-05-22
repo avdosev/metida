e1ef2478d55df7ce8e56972561974bab47cc058a
Sapfir0 <sapfir999999@yandex.ru>
2020-05-22 11:50:09 +0400

    Профиль

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import {
     BrowserRouter as Router, Switch, Route
 } from "react-router-dom";
-import Header from "./Molecules/Header/Header";
 import Index from "./Pages/Index/Index"
 import * as ROUTES from '../config/routes';
 import SignIn from "./Pages/Sign_In/Sign_In";
 import Register from "./Pages/Register/Register";
 import PostPage from "./Pages/Post/PostPage";
+import Profile from "./Pages/Profile/Profile";
 
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         <Router>
             <React.StrictMode>
                 <Switch>
+                    <Route path={ROUTES.PROFILE} component={Profile}/>
                     <Route path={ROUTES.POST} component={PostPage}/>
                     <Route path={ROUTES.SIGN_IN} component={SignIn}/>
                     <Route path={ROUTES.REGISTER} component={Register}/>
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,3 +2,4 @@ export const LANDING = '/';
 export const REGISTER = '/register';
 export const SIGN_IN = '/sign_In';
 export const POST = '/post/:id'
+export const PROFILE = '/author/:name'
