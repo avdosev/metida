9dff9bb351f8786b8f59c17a097e1e4ddcb97a3b
Sapfir0 <sapfir999999@yandex.ru>
2020-05-26 23:23:50 +0400

    Работа с профилем

--- client/src/components/Pages/Sign_In/Sign_In.tsx
+++ client/src/components/Pages/Sign_In/Sign_In.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Sign_InForm from "../../Organisms/Sign_InForm/Sign_InForm";
 import SimplePage from "../../Templates/SimpleTemplate";
 import Header from "../../Molecules/Header/Header";
-import Footer from "../../Organisms/Footer/Footer";
+
 
 export default function SignIn() {
     document.title = "Sign in";
