169074e7cf6794789bd9a8403475b184040258d3
Sapfir0 <sapfir999999@yandex.ru>
2020-05-30 01:30:56 +0400

    Комменты работают получше

--- client/src/components/IComment.ts
+++ client/src/components/IComment.ts
@@ -1,22 +1,31 @@
-import React from "react";
+import {initialUser, IUser} from "./Organisms/IUser";
 
 interface IComments {
     id: number,
     commentAuthorId: number,
     text: string,
-    articleId: number,
+    articleId: string,
     answeringId: number | null,
     raiting: number | null,
     createdAt: Date,
     updatedAt: Date,
-    user: {
-        username: string,
-        about: null,
-        lastname: null,
-        firstname: null,
-        avatar: null
-    },
+    user: IUser,
     child: Array<IComments> | null
 }
 
+const initialComment: IComments = {
+    id: 0,
+    commentAuthorId: 0,
+    text: "",
+    articleId: "",
+    answeringId: null,
+    raiting: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    user: initialUser,
+    child: null
+}
+
 export type {IComments}
+
+export {initialComment}
