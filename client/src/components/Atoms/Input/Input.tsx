dde7883b285b4f01d881485f210bf26d5c547fb6
Sapfir0 <sapfir999999@yandex.ru>
2020-06-01 20:13:06 +0400

    Nullish operator

--- client/src/components/Atoms/Input/Input.tsx
+++ client/src/components/Atoms/Input/Input.tsx
@@ -14,13 +14,13 @@ export default class Input extends React.Component<IProps, IState> {
 
     render() {
         return <>
-            <h3>{this.props.fieldDescription ? this.props.fieldDescription : this.props.fieldName} </h3>
+            <h3>{this.props.fieldDescription ?? this.props.fieldName} </h3>
             <input
-                id={this.props.fieldId ? this.props.fieldId : this.props.fieldName}
-                className={this.props.fieldClass ? this.props.fieldClass : this.props.fieldName}
-                type={this.props.fieldType ? this.props.fieldType : this.props.fieldName}
+                id={this.props.fieldId ?? this.props.fieldName}
+                className={this.props.fieldClass ?? this.props.fieldName}
+                type={this.props.fieldType ?? this.props.fieldName}
                 name={this.props.fieldName}
-                placeholder={this.props.placeholder ? this.props.placeholder : this.props.fieldName}
+                placeholder={this.props.placeholder ?? this.props.fieldName}
                 required
                 pattern={this.props.regexp}
                 onChange={this.props.validateFunc}
--- client/src/components/Molecules/Profile/Profile.tsx
+++ client/src/components/Molecules/Profile/Profile.tsx
@@ -55,13 +55,13 @@ export default class Profile extends React.Component<IProps, IState> {
                     <div className="profile">
                         <div className="left">
                             <img className="avatar"
-                                src={this.state.user.avatar ? this.state.user.avatar : process.env.PUBLIC_URL + "/img/default/avatar_small.png"}
+                                src={this.state.user.avatar ?? process.env.PUBLIC_URL + "/img/default/avatar_small.png"}
                                 alt="avatar"/>
                         </div>
                         <div className="right">
                             <h3>{this.state.user.username}</h3>
                             <p className="about">Обо мне:
-                                {this.state.user.about ? this.state.user.about : "Ты ничего не узнаешь"}
+                                {this.state.user.about ?? "Ты ничего не узнаешь"}
                             </p>
                         </div>
                     </div>
