d6fa1a9977e53c4358a7abb7e463fad8887e5618
Sapfir0 <sapfir999999@yandex.ru>
2020-06-01 20:13:06 +0400

    Nullish operator

--- client/src/components/Atoms/Textarea/Textarea.tsx
+++ client/src/components/Atoms/Textarea/Textarea.tsx
@@ -14,10 +14,10 @@ export default class Textarea extends React.Component<IProps, IState> {
     render() {
         return <>
             <textarea
-                className={this.props.fieldClass ? this.props.fieldClass : this.props.fieldName}
-                id={this.props.fieldId ? this.props.fieldId : this.props.fieldName}
+                className={this.props.fieldClass ?? this.props.fieldName}
+                id={this.props.fieldId ?? this.props.fieldName}
                 name={this.props.fieldName}
-                placeholder={this.props.placeholder ? this.props.placeholder : this.props.fieldName}
+                placeholder={this.props.placeholder ?? this.props.fieldName}
                 required
                 onChange={this.props.validateFunc}
                 value={this.props.value}
--- client/src/components/Organisms/Form/Form.tsx
+++ client/src/components/Organisms/Form/Form.tsx
@@ -32,10 +32,10 @@ export default class Form extends React.Component<IProps, IState> {
 
     render() {
         return <form
-            className={this.props.className ? this.props.className : "reg"}
+            className={this.props.className ?? "reg"}
             onSubmit={this.onSubmit}
             action={this.props.action}
-            method={this.props.method ? this.props.method : "post"}
+            method={this.props.method ?? "post"}
         >
             {this.props.children}
             <input id="submit" type="submit" className="welcome" value={this.props.buttonName} />
