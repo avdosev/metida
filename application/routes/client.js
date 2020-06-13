4c2ed3312388a7de215be0f5955f00436bc50c63
Sapfir0 <sapfir999999@yandex.ru>
2020-06-14 02:03:29 +0400

    не получилось

--- application/routes/client.js
+++ application/routes/client.js
@@ -9,14 +9,6 @@ export const initClientControllers = (app) => {
         res.sendFile(path.join(buildDir, 'index.html'));
     });
 
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https')
-            res.redirect(`https://${req.header('host')}${req.url}`)
-        else
-            next()
-    })
-
-
 }
 
 export default {initClientControllers}
