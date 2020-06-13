2852d60f89383ab4f41c13fc21852c46e9214dd7
Sapfir0 <sapfir999999@yandex.ru>
2020-06-14 01:48:26 +0400

    Тестим редирект на хттпс

--- application/routes/client.js
+++ application/routes/client.js
@@ -9,6 +9,14 @@ export const initClientControllers = (app) => {
         res.sendFile(path.join(buildDir, 'index.html'));
     });
 
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+
+
 }
 
 export default {initClientControllers}
