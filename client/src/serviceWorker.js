0f20eba6c57a0944a3893b9eba8f5b36281e3a58
Sapfir0 <sapfir999999@yandex.ru>
2020-06-15 23:45:13 +0400

    Попробуем так

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -1,172 +1,47 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
+// This is the "Offline page" service worker
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+const CACHE = "pwabuilder-page";
 
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/offline";
 
-const cacheName = 'static_cache';
-const precacheResources = [
-  'offline',
-  //-- images --
-  'img/logo.png',
-  'img/mobile_menu.png',
-  //-- json --
-  'json/input_errors.json',
-  'json/lexem_table.json',
-];
-
-
-
-export function register(config) {
-  console.log('[ServiceWorker] Register on ' +  process.env.NODE_ENV);
-
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(window.location.href);
-    console.log('[ServiceWorker] was register on ' + publicUrl);
-
-    if (publicUrl.origin !== window.location.origin) {
-      console.warn('[ServiceWorker] publicUrl is not equal locationUrl');
-
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return;
-    }
-
-    window.addEventListener('load', () => {
-      const swUrl = `/service-worker.js`;
-      console.log('[ServiceWorker] Load');
-
-      if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
-          );
-        });
-      } else {
-        // Is not localhost. Just register service worker
-        console.log('[ServiceWorker] Valid Register');
-
-        registerValidSW(swUrl, config);
-      }
-    });
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
   }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+      caches.open(CACHE)
+          .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
 }
 
-function registerValidSW(swUrl, config) {
-  navigator.serviceWorker
-    .register(swUrl)
-    .then(registration => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        console.log("[ServiceWorker] Installing")
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-        if (installingWorker == null) {
-          console.warn("[ServiceWorker] Has no installing worker")
-          return;
+        if (preloadResp) {
+          return preloadResp;
         }
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            console.log("[ServiceWorker] Installed")
 
-            if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
 
-              // Execute callback
-              if (config && config.onUpdate) {
-                console.log("[ServiceWorker] Updating cache callback")
-                config.onUpdate(registration);
-              }
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
-
-              // Execute callback
-              if (config && config.onSuccess) {
-                console.log("[ServiceWorker] Running cache callback")
-                config.onSuccess(registration);
-              }
-            }
-          }
-        };
-      };
-    })
-    .catch(error => {
-      console.error('Error during service worker registration:', error);
-    });
-}
-
-function checkValidServiceWorker(swUrl, config) {
-  // Check if the service worker can be found. If it can't reload the page.
-  console.log("[ServiceWorker] Check for valid ")
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
-  })
-    .then(response => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      console.log("[ServiceWorker] Get valid SW ")
-      const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
-      } else {
-        // Service worker found. Proceed as normal.
-        registerValidSW(swUrl, config);
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
       }
-    })
-    .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      );
-    });
-}
-
-export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready
-      .then(registration => {
-        registration.unregister();
-      })
-      .catch(error => {
-        console.error(error.message);
-      });
+    })());
   }
-}
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -105,6 +105,24 @@ module.exports = env => {
                 // and not allow any straggling "old" SWs to hang around
                 clientsClaim: true,
                 skipWaiting: true,
+                exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+                // Define runtime caching rules.
+                runtimeCaching: [{
+                    // Match any request that ends with .png, .jpg, .jpeg or .svg.
+                    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+                    // Apply a cache-first strategy.
+                    handler: 'CacheFirst',
+
+                    options: {
+                        // Use a custom cache name.
+                        cacheName: 'images',
+                        expiration: {
+                            maxEntries: 20,
+                        },
+                    },
+                }],
             }),
         ]
     }
