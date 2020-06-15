fd07806413e7d811dbe9b28c4cc559c30d70aeaf
Sapfir0 <sapfir999999@yandex.ru>
2020-06-15 22:35:05 +0400

    Небольшое изменение с воркерами

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./store"
 
@@ -16,10 +15,3 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === "production") {
-    serviceWorker.register();
-}
