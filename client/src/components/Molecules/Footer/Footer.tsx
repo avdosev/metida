193c674388d1cad251e175d1fb26e8550951f939
Sapfir0 <sapfir999999@yandex.ru>
2020-06-23 09:39:31 +0400

    Некие фиксы темы и еще кое-что

--- client/src/components/Molecules/Footer/Footer.tsx
+++ client/src/components/Molecules/Footer/Footer.tsx
@@ -3,5 +3,5 @@ import ThemeChanger from "../../Atoms/ThemeChanger/ThemeChanger";
 import style from "./Footer.scss"
 
 export default function Footer() {
-    return(<footer className={style.footer}> <ThemeChanger /> Все права за K.Corp.</footer>)
+    return(<footer className={style.footer}> Все права за K.Corp. <ThemeChanger /> </footer>)
 }
