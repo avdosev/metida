4d1ead0ad94be8823aef3a8412282657a90eedd4
Sapfir0 <sapfir999999@yandex.ru>
2020-06-25 18:36:25 +0400

    Топовые фиксы

--- client/src/components/Molecules/Field/FieldInput.tsx
+++ client/src/components/Molecules/Field/FieldInput.tsx
@@ -1,30 +1,21 @@
 import React from "react";
 import ErrorPlaceholder from "../../Atoms/ErrorPlaceholder/ErrorPlaceholder";
 import Input from "../../Atoms/Input/Input";
-import {ITextFieldErrored} from "./IField";
+import { ITextFieldErrored } from "./IField";
 
-interface IProps extends ITextFieldErrored {
-}
+interface IProps extends ITextFieldErrored {}
 
 export default function FieldInput(props: IProps) {
-    const valid = props.validate(props.value)
-    
-    return (<>
-        <Input
-            autofocus={props.autofocus}
-            fieldId={props.fieldId}
-            fieldClass={props.fieldClass}
-            fieldName={props.fieldName}
-            onChange={props.onChange}
-            regexp={props.regexp}
-            fieldType={props.fieldType}
-            fieldDescription={props.fieldDescription}
-            placeholder={props.placeholder}
-            value={props.value}
-        />
-        <ErrorPlaceholder valid={valid} value={props.errorText} showStrategy={props.showErrorStrategy} />
-    </>);
-    
+  const valid = props.validate(props.value);
+
+  return (
+    <>
+      <Input {...props} />
+      <ErrorPlaceholder
+        valid={valid}
+        value={props.errorText}
+        showStrategy={props.showErrorStrategy}
+      />
+    </>
+  );
 }
-
-
