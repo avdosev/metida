ca5ee1b1a7ecda6476e73bb4b2a5c2305459d99b
Sapfir0 <sapfir999999@yandex.ru>
2020-06-30 12:50:16 +0400

    Бля ебучий стайл

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
+  // "setupFiles": [
+  //   "<rootDir>/test/setup.js"
+  // ],
   // Stop running tests after `n` failures
   // bail: 0,
 
