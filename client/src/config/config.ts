9d07fdc80572fa211dabc45b537a0b5dd3654c75
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2020-06-30 14:34:26 +0400

    Dev (#85)
    * Чутка поправил тесты формы входа, теперь я мокаю фетч
    * Фикс скелетона ленты
    * Логгер для сервера чет не оч

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,7 +1,7 @@
 console.log(process.env.NODE_ENV);
 let serverUri: string;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
     const host = 'localhost';
     const serverPort = 7080;
     serverUri = `http://${host}:${serverPort}`;
@@ -9,5 +9,6 @@ if (process.env.NODE_ENV === 'development') {
     serverUri = '';
 }
 
+console.log(serverUri);
 export { serverUri };
 export const lexTableUrl = '/json/lexem_table.json';
